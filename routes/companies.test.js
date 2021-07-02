d4b7cf3f4f21de5a32d63ce23f60ad5e519f3801
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-01 20:49:06 -0700

    wrote a test to check that we can search by multiple fields at once

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -148,6 +148,21 @@ describe("GET /companies", function () {
     })
   });
 
+  test('a user is able to search by minimum number of employees and name', async () => {
+    const resp = await request(app).get('/companies?minEmployees=3&name=C1');
+    expect(resp.body).toEqual({
+      companies: [
+        {
+          handle: "c3",
+          name: "C3",
+          description: "Desc3",
+          numEmployees: 3,
+          logoUrl: "http://c3.img",
+        }
+      ]
+    })
+  });
+
   test("fails: test next() handler", async function () {
     // there's no normal failure event which will cause this route to fail ---
     // thus making it hard to test that the error-handler works with it. This
