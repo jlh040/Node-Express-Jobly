242d27be6037d2b5e0390f069dfae8887b7d818a
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-03 14:07:24 -0700

    wrote boilerplace for job model test file

--- models/job.test.js
+++ models/job.test.js
@@ -2,5 +2,23 @@
 
 const db = require('../db');
 const { BadRequestError, NotFoundError } = require("../expressError");
-// const Job = require('./job.js');
+const Job = require('./job.js');
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+} = require("./_testCommon");
 
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** create */
+
+describe('create a new job', () => {
+  test('works', async () => {
+    expect(1).toBe(1);
+  })
+})
