258bcd7288ab1ef18e2c003d21ac3caa5b391c38
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-04 10:21:51 -0700

    implemented the GET route for finding a particular job

--- routes/jobs.js
+++ routes/jobs.js
@@ -23,32 +23,32 @@ const router = new express.Router();
  * returns { id, title, salary, equity, companyHandle }
  * 
  * Authorization required: admin
- */
+*/
 
- router.post('/', ensureLoggedIn, async (req, res ,next) => {
-    try {
-        const validator = jsonschema.validate(req.body, jobNewSchema);
-        if (!validator.valid) {
-          const errs = validator.errors.map(e => e.stack);
-          throw new BadRequestError(errs);
-        }
-    
-        const job = await Job.create(req.body);
-        return res.status(201).json({ job });
-      } catch (err) {
-        return next(err);
-      }
- });
+router.post('/', ensureLoggedIn, async (req, res ,next) => {
+try {
+    const validator = jsonschema.validate(req.body, jobNewSchema);
+    if (!validator.valid) {
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs);
+    }
 
- /** GET / => 
-  *     { jobs: [ { id, title, salary, equity, companyHandle }, ...] }
-  * 
-  * Can filter on provided search filters:
-  *     - title
-  *     - minSalary
-  *     - hasEquity
-  * 
-  * Authorization required: none
+    const job = await Job.create(req.body);
+    return res.status(201).json({ job });
+    } catch (err) {
+    return next(err);
+    }
+});
+
+/** GET / => 
+ *     { jobs: [ { id, title, salary, equity, companyHandle }, ...] }
+ * 
+ * Can filter on provided search filters:
+ *     - title
+ *     - minSalary
+ *     - hasEquity
+ * 
+ * Authorization required: none
  */
 
 router.get("/", async function (req, res, next) {
@@ -64,7 +64,27 @@ router.get("/", async function (req, res, next) {
     } catch (err) {
       return next(err);
     }
-  });
+});
+
+/** GET /:id => {job}
+ * 
+ * Job is { id, title, salary, equity, companyHandle }
+ * 
+ * Authorization required: none
+*/
+
+router.get("/:id", async function (req, res, next) {
+    try {
+      const job = await Job.get(req.params.id);
+      return res.json({ job });
+    } catch (err) {
+      return next(err);
+    }
+});
+
+
+
+
 
 
 
