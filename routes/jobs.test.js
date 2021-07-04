6166e027d0cefad0bbcac583f23b02aad1dca9cf
Jeff Hall <jeff.leon.hall@gmail.com>
2021-07-03 17:29:44 -0700

    wrote a test to check that an admin can create a new job using the POST route

--- routes/jobs.test.js
+++ routes/jobs.test.js
@@ -20,3 +20,28 @@ beforeEach(commonBeforeEach);
 afterEach(commonAfterEach);
 afterAll(commonAfterAll);
 
+/********************************** POST /jobs */
+
+describe('POST /jobs', function() {
+    const newJob = {
+        title: 'someNewJob',
+        salary: 99000,
+        equity: 0,
+        companyHandle: 'c1'
+    };
+
+    test("an admin can create a job", async function () {
+        const resp = await request(app)
+            .post("/jobs")
+            .send(newJob)
+            .set("authorization", `Bearer ${u1Token}`);
+        expect(resp.statusCode).toEqual(201);
+        expect(resp.body).toEqual({
+          job: {
+              ...newJob,
+              equity: '0',
+              id: expect.any(Number)
+          },
+        });
+    });
+})
